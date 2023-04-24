67adec445fb3b0b4cc3fa1bb6a566a29a62c7e5a
saichandanal <saichandana2021@gmail.com>
2023-04-24 11:09:10 +0530

    getting parameters from settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -68,3 +68,9 @@ object Build : BuildType({
         }
     }
 })
+
+object MyVcsRoor: GitVcsRoot({
+    name = DslContext.getParameter("vcsName")
+    url = DslContext.getParameter("vcsUrl")
+    branch = DslContext.getParameter("vcsBranch", "refs/heads/main")
+})
