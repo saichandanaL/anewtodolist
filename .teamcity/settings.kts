1296dede4ad06570394dc0f39bd8a97911f6d011
saichandanal <saichandana2021@gmail.com>
2023-04-24 13:44:52 +0530

    settings from other repo

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.triggers.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
