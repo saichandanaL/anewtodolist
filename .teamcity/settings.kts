4ec5855be28d026d1965b7872f017867d49dc41d
saichandanal <saichandana2021@gmail.com>
2023-04-24 14:49:22 +0530

    settings repo

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,6 +4,7 @@ import jetbrains.buildServer.configs.kotlin.buildFeatures.xmlReport
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.ui.changeVcsRoot
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 /*
@@ -39,6 +40,10 @@ project {
 
 object Build : BuildType({
     name = "Build"
+
+    vcs {
+        root(Myvcsroot)
+    }
 
     artifactRules = "target/*.jar"
 
